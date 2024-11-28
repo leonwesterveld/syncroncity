ac5cceebc6224e7b17e376d3068e9aeccd53833f
leonwesteveld <leonwesterveld12@gmail.com>
2024-11-28 13:04:35 +0100

    knoppen bewerkt kleur dropdown heeft juiste info

--- syncroncity/app/index.jsx
+++ syncroncity/app/index.jsx
@@ -5,26 +5,27 @@ import { Picker } from "@react-native-picker/picker";
 export default function Index() {
   const [selectedValue, setSelectedValue] = useState("");
   const [ddValue, setDdValue] = useState("");
+  const [selectedButton, setSelectedButton] = useState("");
+  const handleButtonPress = (value) => {
+    setSelectedButton(value);
+    setDdValue(value);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.section}>
-        <Button
+      <Button
           title="denk"
-          onPress={() => setDdValue("denk")}
+          onPress={() => handleButtonPress("denk")}
+          color={selectedButton === "denk" ? "#3498db" : "#7f8c8d"}
         />
         <Button
           title="droom"
-          onPress={() => setDdValue("droom")}
+          onPress={() => handleButtonPress("droom")}
+          color={selectedButton === "droom" ? "#3498db" : "#7f8c8d"}
         />
         </View>
-      
 
-        <View style={styles.section}>
-      <Text style={styles.selectedText}>
-        You selected: {ddValue}
-      </Text>
-      </View>
       
 
       <Picker
@@ -32,14 +33,14 @@ export default function Index() {
         style={styles.picker}
         onValueChange={(itemValue) => setSelectedValue(itemValue)}
       >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="javascript" />
-        <Picker.Item label="Python" value="python" />
-        <Picker.Item label="C++" value="c++" />
+        <Picker.Item label="zo juist" value="zo juist" />
+        <Picker.Item label="1 uur geleden" value="1 uur geleden" />
+        <Picker.Item label="2 uur geleden" value="2 uur geleden" />
+        <Picker.Item label="vandaag" value="vandaag" />
       </Picker>
 
       <Text style={styles.selectedText}>
-        You selected: {selectedValue}
+        {selectedValue}
       </Text>
     </View>
   );
